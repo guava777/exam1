ed7fc127d5b73d8f6e50d2b43f0b85648959e568
guava777 <123084987+guava777@users.noreply.github.com>
2023-04-17 12:17:14 +0800

    Update stim.h

--- exam1-p1/stim.h
+++ exam1-p1/stim.h
@@ -6,7 +6,6 @@ using std::setw;
 #include "adder.h"
 #include "ready_valid_port.h"
 
-
 const float x_input_signal={0.500,   0.525,   0.549,   0.574,   0.598,   0.622,   0.646,   0.670,
                             0.693,   0.715,   0.737,   0.759,   0.780,   0.800,   0.819,   0.838,
                             0.856,   0.873,   0.889,   0.904,   0.918,   0.931,   0.943,   0.954,
@@ -29,21 +28,34 @@ SC_MODULE(stim) {
   sc_in_clk i_clk;
   sc_out<bool> o_rst;
   //o_a output ports: message, ready, valid signals
-  rdyvld_port_out<sc_uint<WIDTH> > o_a;
+  rdyvld_port_out<sc_ufixed_fast<4, 1> > o_a;
+  //o_b output ports: message, ready, valid signals
+  rdyvld_port_out<sc_ufixed_fast<4, 1> > o_b;
   //i_sum input ports: message, ready, valid signals
-  rdyvld_port_in<sc_uint<WIDTH+1> > i_sum;
+  rdyvld_port_in<sc_ufixed_fast<4, 1> > i_sum;
   //Store the previous inputs to FIFOs
-  sc_uint<WIDTH> t_a;
+  sc_ufixed_fast<4, 1> t_a;
   sc_uint<1> t_a_rdy;
   sc_uint<1> t_a_vld;
-  sc_uint<WIDTH+1> t_sum;
+  sc_ufixed_fast<4, 1> t_b;
+  sc_uint<1> t_b_rdy;
+  sc_uint<1> t_b_vld;
+  sc_ufixed_fast<4, 1> t_sum;
   sc_uint<1> t_sum_rdy;
   sc_uint<1> t_sum_vld;
 
-  void stim_gen() {    
-    for (int a = 0; a < 128; a++) {
-        wait();
+  void stim_gen() {
+    //initilize handshaking signals
+    //o_a.vld.write(false); 
+    //o_b.vld.write(false); 
+    //i_sum.rdy.write(false);
+    for (int a = 0; a < 128; a= a+2) {
         o_a.write(x_input_signal[a]);
+        o_b.write(x_input_signal[a+1]);
+
+        cout << setw(12) << sc_time_stamp();
+
+        wait();
     }
   }
 
@@ -55,12 +67,18 @@ SC_MODULE(stim) {
   }
 
   void monitor() {
-    cout << setw(8) << "sum vld" << endl;
+    cout << setw(8) << "time" << setw(8) << "a" << setw(8) << "a rdy" << setw(8) << "a vld" << setw(8) << "b" 
+         << setw(8) << "b rdy" << setw(8) << "b vld" 
+         << setw(8) << "sum" << setw(8) << "sum rdy" 
+         << setw(8) << "sum vld" << endl;
     while (true) {
       wait();
       t_a=o_a.msg.read();
       t_a_rdy=o_a.rdy.read();
       t_a_vld=o_a.vld.read();
+      t_b=o_b.msg.read();
+      t_b_rdy=o_b.rdy.read();
+      t_b_vld=o_b.vld.read();
       t_sum=i_sum.msg.read();
       t_sum_rdy=i_sum.rdy.read();
       t_sum_vld=i_sum.vld.read();
@@ -68,6 +86,9 @@ SC_MODULE(stim) {
       cout << setw(8) << t_a.to_string(SC_BIN);
       cout << setw(8) << t_a_rdy.to_string(SC_BIN);
       cout << setw(8) << t_a_vld.to_string(SC_BIN);
+      cout << setw(8) << t_b.to_string(SC_BIN);
+      cout << setw(8) << t_b_rdy.to_string(SC_BIN);
+      cout << setw(8) << t_b_vld.to_string(SC_BIN);
       cout << setw(8) << t_sum.to_string(SC_BIN);
       cout << setw(8) << t_sum_rdy.to_string(SC_BIN);
       cout << setw(8) << t_sum_vld.to_string(SC_BIN) << endl;
