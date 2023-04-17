10a94038918633dfd9124e54d215e02ba393f481
guava777 <123084987+guava777@users.noreply.github.com>
2023-04-17 12:17:56 +0800

    Update adder.h

--- exam1-p1/adder.h
+++ exam1-p1/adder.h
@@ -8,30 +8,36 @@ using namespace std;
 using namespace sc_dt;
 using namespace sc_core;
 
-#include "ready_valid_port.h"
+const int WIDTH = 2;
 
+#include "ready_valid_port.h"
 
 class adder : public sc_module {
 public:
   sc_in_clk i_clk;
   sc_in<bool> i_rst;
-  rdyvld_port_in<float> i_a_port;
-  rdyvld_port_out<float> o_sum_port;
+  //i_a input ports: message, ready, valid signals
+  rdyvld_port_in<sc_uint<WIDTH> > i_a_port;
+  //i_b input ports: message, ready, valid signals
+  rdyvld_port_in<sc_uint<WIDTH> > i_b_port;
+  //o_sum output ports: message, ready, valid signals
+  rdyvld_port_out<sc_uint<WIDTH+1> > o_sum_port;
 
   void do_add() {
     //initilize handshaking signals
     i_a_port.rdy.write(false); 
+    i_b_port.rdy.write(false); 
     o_sum_port.vld.write(false);
     temp = 0;
-
     while (true) {
-      x[0] = temp;
-      x[1] = i_a_port.read();
-      x[2] = i_a_port.read();
-      temp = x[2];
-      float _o_sum = x[2]/2 + x[1]/3 + x[0]/6;
-      o_sum_port.write(_o_sum);
-      wait();
+        _i_a = i_a_port.read();
+        _i_b = i_b_port.read();
+        _i_c = temp;
+        temp = _i_a;
+        wait();
+        _o_sum = _i_a/2 + _i_b/3 + _i_c/6;
+        o_sum_port.write(_o_sum);
+        wait();
     }
   }
 
@@ -44,15 +50,16 @@ public:
     //reset_signal_is(i_rst, false); 
   }
 
-  sc_ufixed_fast<4,1,SC_TRN, SC_WRAP,2> i_a() { return _i_a; }
-  sc_ufixed_fast<4,1,SC_TRN, SC_WRAP,2> o_sum() { return _o_sum; }
+  sc_uint<WIDTH> i_a() { return _i_a; }
+  sc_uint<WIDTH> i_b() { return _i_b; }
+  sc_uint<WIDTH+1> o_sum() { return _o_sum; }
 
 private:
   const int id;
-  sc_ufixed_fast<4,1,SC_TRN, SC_WRAP,2> _i_a;
-  sc_ufixed_fast<4,1,SC_TRN, SC_WRAP,2> _o_sum;
-  float temp;
-  sc_ufixed_fast<4,1,SC_TRN, SC_WRAP,2> x[3];
+  sc_uint<WIDTH> _i_a;
+  sc_uint<WIDTH> _i_b;
+  sc_uint<WIDTH + 1> _o_sum;
+
  
 };
 
